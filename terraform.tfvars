893df048f86fa3cae13253b1d82e15ad74e67d82
vamshidherponnala <108470279+vamshidherponnala@users.noreply.github.com>
2022-07-01 19:07:38 +0530

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+prefix="VNET"
+vent_cidr_prefix = "10.20.0.0/16"
+
+
+
---
+++ variables.tf
@@ -0,0 +1,13 @@
+ variable "prefix" {
+    type = string
+    description = "used for define standard prefix for all resource"    
+}
+
+variable "vent_cidr_prefix" {
+    type = string
+    description = "This variable defines address space for vnet"
+  
+}
+
+
+
